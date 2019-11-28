15186d35dd3d38a6ebfe77133460b6b3f3cb6a5d
Hrabur Trendafilov <hrabur@yahoo.com>
2019-11-28 11:05:03 +0200

    Updates

---
+++ precache-manifest.96dfb1e7d7da1e622a1d4384b32e2d53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2377e69b5546ccd6937",
+    "url": "/quarkus-vs-spring-slides/static/js/runtime~main.c2377e69.js"
+  },
+  {
+    "revision": "9c169f0e6748624138ec",
+    "url": "/quarkus-vs-spring-slides/static/js/main.9c169f0e.chunk.js"
+  },
+  {
+    "revision": "9a235d02c1fb78353f33",
+    "url": "/quarkus-vs-spring-slides/static/js/1.9a235d02.chunk.js"
+  },
+  {
+    "revision": "9a235d02c1fb78353f33",
+    "url": "/quarkus-vs-spring-slides/static/css/1.50f8a891.chunk.css"
+  },
+  {
+    "revision": "c7c4848ee421ece2b1feb42eee9528f3",
+    "url": "/quarkus-vs-spring-slides/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/quarkus-vs-spring-slides/precache-manifest.96dfb1e7d7da1e622a1d4384b32e2d53.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/quarkus-vs-spring-slides/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
