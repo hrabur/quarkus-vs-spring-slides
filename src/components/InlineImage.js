611d7b53d73690bfb0b090c07aff94d9c2d79e70
Hrabur Trendafilov <hrabur@yahoo.com>
2019-11-29 12:17:24 +0200

    Add slides and images

---
+++ src/components/InlineImage.js
@@ -0,0 +1,9 @@
+import styled from "react-emotion";
+import { Image } from "spectacle";
+
+const InlineImage = styled(Image)`
+  display: inline-block;
+  vertical-align: text-bottom;
+`;
+
+export default InlineImage;
--- src/presentation.js
+++ src/presentation.js
@@ -1,20 +1,26 @@
 import React from "react";
 import {
-  BlockQuote,
-  Cite,
   Deck,
   Heading,
-  ListItem,
-  List,
-  Quote,
-  Slide,
   Text,
-  Image,
-  Appear
+  Fit,
+  Layout,
+  Markdown,
+  Table,
+  TableHeader,
+  TableHeaderItem,
+  TableRow,
+  TableBody,
+  TableItem,
+  Link,
+  Appear,
+  List,
+  ListItem
 } from "spectacle";
 import createTheme from "spectacle/lib/themes/default";
 import preloader from "spectacle/lib/utils/preloader";
 import PlovDevSlide from "./components/PlovDevSlide";
+import InlineImage from "./components/InlineImage";
 
 require("normalize.css");
 const theme = createTheme(
@@ -43,6 +49,8 @@ export default class Presentation extends React.Component {
   render() {
     return (
       <Deck
+        contentHeight="1000px"
+        contentWidth="1200px"
         transition={["zoom", "slide"]}
         transitionDuration={500}
         theme={theme}
@@ -72,15 +80,99 @@ export default class Presentation extends React.Component {
           textColor="tertiary"
           bgImage={images.cloudComputingServices}
         ></PlovDevSlide>
-        <PlovDevSlide
-          transition={["fade"]}
-          bgColor="secondary"
-          textColor="primary"
-        >
-          <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
-          </BlockQuote>
+        <PlovDevSlide transition={["fade"]}>
+          <Heading size={4}>
+            <InlineImage src={images.springLogo} height="4rem" /> Spring Boot
+          </Heading>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHeaderItem />
+                <TableHeaderItem>
+                  <InlineImage src={images.springLogo} height="2rem" /> Spring
+                  Boot
+                </TableHeaderItem>
+                <TableHeaderItem>
+                  <InlineImage src={images.quarkusLogo} height="2rem" /> Quarkus
+                </TableHeaderItem>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <Appear>
+                <TableRow>
+                  <TableItem>Start in seconds</TableItem>
+                  <TableItem>
+                    <Link href="https://start.spring.io/" target="_blank">
+                      Spring Initializr
+                    </Link>
+                  </TableItem>
+                  <TableItem>
+                    <Link href="https://code.quarkus.io/" target="_blank">
+                      Start Coding
+                    </Link>
+                  </TableItem>
+                </TableRow>
+              </Appear>
+              <Appear>
+                <TableRow>
+                  <TableItem>Build anything</TableItem>
+                  <TableItem>
+                    REST API, WebSocket, web, streaming, tasks, and more
+                  </TableItem>
+                  <TableItem>
+                    REST API, WebSocket, web, streaming, tasks, and more
+                  </TableItem>
+                </TableRow>
+              </Appear>
+              <Appear>
+                <TableRow>
+                  <TableItem>Simplified security</TableItem>
+                  <TableItem>Spring Security</TableItem>
+                  <TableItem>Elytron Security</TableItem>
+                </TableRow>
+              </Appear>
+              <Appear>
+                <TableRow>
+                  <TableItem>SQL and NoSQL</TableItem>
+                  <TableItem>All SQL, MongoDb, Neo4j, Rerdris, etrc.</TableItem>
+                  <TableItem>All SQL, MongoDb, Neo4j, etc.</TableItem>
+                </TableRow>
+              </Appear>
+              <Appear>
+                <TableRow>
+                  <TableItem>Embedded runtime</TableItem>
+                  <TableItem>Tomcat, Jetty, and Undertow</TableItem>
+                  <TableItem>Vert.x and Netty at its core</TableItem>
+                </TableRow>
+              </Appear>
+              <Appear>
+                <TableRow>
+                  <TableItem>Developer productivity</TableItem>
+                  <TableItem>
+                    Auto-config, LiveReload and Auto Restart
+                  </TableItem>
+                  <TableItem>Zero-config, live reload</TableItem>
+                </TableRow>
+              </Appear>
+              <Appear>
+                <TableRow>
+                  <TableItem>Production-ready features</TableItem>
+                  <TableItem>
+                    Spring Actuator (tracing, metrics, and health)
+                  </TableItem>
+                  <TableItem>
+                    Microprofile Health and Metrics, OpenTracing
+                  </TableItem>
+                </TableRow>
+              </Appear>
+            </TableBody>
+          </Table>
+        </PlovDevSlide>
+        <PlovDevSlide>
+          <Heading size={3}>Quarkus</Heading>
+          <List>
+            <ListItem>Build om top of </ListItem>
+          </List>
         </PlovDevSlide>
       </Deck>
     );
